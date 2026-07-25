b4428426f4132762bc9dab189b627472d21fcced
Sergei Golubchik <serg@mariadb.org>
2026-07-25 13:17:26 +0200

    PR-MDEV autolinking: also do it in commit view

--- MDEVs-auto-linking-in-PRs.user.js
+++ MDEVs-auto-linking-in-PRs.user.js
@@ -2,11 +2,12 @@
 // @name        MDEVs in PRs
 // @namespace   https://github.com/vuvova
 // @include     https://github.com/MariaDB/server/pull/*
+// @include     https://github.com/MariaDB/server/commit/*
 // @version     1
 // @grant       none
 // ==/UserScript==
 
-var spans = document.getElementsByClassName("markdown-title");
+var spans = document.querySelectorAll('.markdown-title, [class^="CommitHeader-module__commitMessageContainer__"]');
 for (var i = 0; i < spans.length; i++) {
   spans[i].innerHTML = spans[i].innerHTML.replace(/MDEV-\d+/g, '<a href="https://jira.mariadb.org/browse/$&">$&</a>');
 }
